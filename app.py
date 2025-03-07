63bb1a319e46a1f50ef6e0052aa69dd6c9809817
Dilli Ram Shahu <dilliramshah4@gmail.com>
2025-03-07 18:33:20 +0530

    update app.py to allow external access by setting host and port in run configuration

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def delete(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Allow external access
